a336a08e5219cdfa588e84902a783e6bc0a49134
TitanioRvzz <luchoxo@hotmail.es>
2024-11-19 15:08:53 -0300

    Tiles-UPDATE

---
+++ Assets/Mov3.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+public class Mov3 : MonoBehaviour
+{
+    [SerializeField] float Speed;
+    [SerializeField] float Radio0;
+    [SerializeField] Vector2 offsetpunto;
+    [SerializeField] LayerMask obstaculo;
+    [SerializeField] LayerMask aguaLayer;
+    [SerializeField] LayerMask tortugaLayer;
+    [SerializeField] Tilemap terrenoTilemap;
+    [SerializeField] TileBase arenaSecaTile;
+    [SerializeField] TileBase arenaMojadaTile;
+    [SerializeField] Slider humedadSlider;
+    [SerializeField] Animator anim;
+
+    private Vector3Int targetCell;
+    private bool moviendo = false;
+
+    private void Start()
+    {
+        targetCell = terrenoTilemap.WorldToCell(transform.position);
+        humedadSlider.value = 0;
+    }
+
+    private void Update()
+    {
+        if (!moviendo)
+        {
+            // Capturar input de movimiento
+            Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            // Asegurar movimiento en solo una dirección a la vez
+            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+            {
+                input.y = 0;
+            }
+            else
+            {
+                input.x = 0;
+            }
+
+            // Si se presiona una tecla válida, intentar mover
+            if (input != Vector2.zero)
+            {
+                Vector3Int moveDirection = new Vector3Int(Mathf.RoundToInt(input.x), Mathf.RoundToInt(input.y), 0);
+                Vector3Int newCell = targetCell + moveDirection;
+
+                // Verificar colisión con obstáculos
+                if (!Physics2D.OverlapCircle(terrenoTilemap.GetCellCenterWorld(newCell) + (Vector3)offsetpunto, Radio0, obstaculo))
+                {
+                    targetCell = newCell;
+                    StartCoroutine(MoveToCell(terrenoTilemap.GetCellCenterWorld(newCell)));
+                }
+
+                // Actualizar animación y orientación
+                if (input.x != 0)
+                {
+                    anim.SetBool("Walk", true);
+                    gameObject.GetComponent<SpriteRenderer>().flipX = input.x < 0;
+                }
+                else if (input.y != 0)
+                {
+                    anim.SetBool("Walk", true);
+                }
+            }
+            else
+            {
+                anim.SetBool("Walk", false);
+            }
+        }
+
+        // Manejo del agua
+        if (Physics2D.OverlapCircle(transform.position, Radio0, aguaLayer))
+        {
+            humedadSlider.value = humedadSlider.maxValue;
+        }
+    }
+
+    private IEnumerator MoveToCell(Vector3 targetPosition)
+    {
+        moviendo = true;
+
+        while (Vector3.Distance(transform.position, targetPosition) > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, Speed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        moviendo = false;
+
+        // Actualizar tiles al llegar a la nueva posición
+        UpdateTile();
+    }
+
+    private void UpdateTile()
+    {
+        Vector3Int currentCell = terrenoTilemap.WorldToCell(transform.position);
+        TileBase currentTile = terrenoTilemap.GetTile(currentCell);
+
+        if (currentTile == arenaSecaTile && humedadSlider.value > 0)
+        {
+            humedadSlider.value -= 0.2f; // Reducir humedad
+            terrenoTilemap.SetTile(currentCell, arenaMojadaTile); // Cambiar a tile mojado
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + (Vector3)offsetpunto, Radio0);
+    }
+}
--- Assets/Turtle2.cs
+++ Assets/Turtle2.cs
@@ -1,18 +1,90 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Turtle2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Tilemap terrenoTilemap;
+    [SerializeField] RuleTile arenaMojadaRuleTile;
+    [SerializeField] TileBase aguaTile;
+    [SerializeField] float moveInterval = 2f;
+
+    private Vector3Int currentCell;
+    private bool isMovingToWater = false;
+
+    private void Start()
     {
-        
+        currentCell = terrenoTilemap.WorldToCell(transform.position);
+        StartCoroutine(MoveRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator MoveRoutine()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(moveInterval);
+
+            if (!isMovingToWater)
+            {
+                CheckForMojadaTiles();
+                if (!isMovingToWater)
+                {
+                    MoveRandom();
+                }
+            }
+            else
+            {
+                MoveToWater();
+            }
+        }
+    }
+
+    private void MoveRandom()
+    {
+        Vector3Int[] directions = { Vector3Int.up, Vector3Int.down, Vector3Int.left, Vector3Int.right };
+        Vector3Int randomDirection = directions[Random.Range(0, directions.Length)];
+        Vector3Int targetCell = currentCell + randomDirection;
+
+        if (terrenoTilemap.HasTile(targetCell))
+        {
+            currentCell = targetCell;
+            transform.position = terrenoTilemap.GetCellCenterWorld(targetCell);
+        }
+    }
+
+    private void CheckForMojadaTiles()
+    {
+        Vector3Int[] directions = { Vector3Int.up, Vector3Int.down, Vector3Int.left, Vector3Int.right };
+        foreach (var direction in directions)
+        {
+            Vector3Int adjacentCell = currentCell + direction;
+            if (terrenoTilemap.GetTile(adjacentCell) == arenaMojadaRuleTile)
+            {
+                isMovingToWater = true;
+                return;
+            }
+        }
+    }
+
+    private void MoveToWater()
+    {
+        Vector3Int[] directions = { Vector3Int.up, Vector3Int.down, Vector3Int.left, Vector3Int.right };
+        foreach (var direction in directions)
+        {
+            Vector3Int adjacentCell = currentCell + direction;
+            if (terrenoTilemap.GetTile(adjacentCell) == aguaTile)
+            {
+                // Acción de salvación
+                SaveTurtle();
+                return;
+            }
+        }
+    }
+
+    private void SaveTurtle()
+    {
+        Debug.Log("Tortuga salvada!");
+        Destroy(gameObject);
+        // Aquí puedes sumar puntos o activar animaciones
     }
 }
