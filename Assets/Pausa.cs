de53ca050936371eab1abfe43952c50ac5ae30bb
Nic292 <gonzalezlopez.ngl@gmail.com>
2024-12-04 12:08:57 -0300

    Menu Pausa

---
+++ Assets/Pausa.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausa : MonoBehaviour
+{
+    [SerializeField] GameObject botonPausa;
+    [SerializeField] GameObject menuPausa;
+  
+   private void Update()
+   {
+   if(Input.GetKeyDown(KeyCode.Escape))
+   {
+    pausa();
+   } 
+   }
+public void pausa()
+{
+
+Time.timeScale = 0f;
+botonPausa.SetActive(false);
+menuPausa.SetActive(true);
+}
+
+public void Reaundar()
+{
+    
+    Time.timeScale = 1f;
+    botonPausa.SetActive(true);
+    menuPausa.SetActive(false);
+}
+public void salir()
+{
+  SceneManager.LoadScene(1);
+}
+public void reiniciar()
+{
+   
+Time.timeScale = 1f;
+SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+}
+
+}
